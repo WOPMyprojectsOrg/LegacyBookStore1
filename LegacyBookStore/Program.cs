fa076bd1b62e0c2082e06a7c321b75d655aa720f
Андрей Максимович Коробков <144508844+Korobkov124@users.noreply.github.com>
2025-10-01 11:48:44 +0700

    Added swagger to root directory

--- LegacyBookStore/Program.cs
+++ LegacyBookStore/Program.cs
@@ -7,8 +7,18 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 var app = builder.Build();
-
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    });
+}
 
 app.Use(async (context, next) =>
 {
