31ca628a54cfc1ebc67667c445ea6d30cab3c957
Ilia <ilia_shcherbakov1234@mail.ru>
2025-10-02 19:45:13 +0700

    Add DTO

---
+++ LegacyBookStore/Models/CreateBookRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LegacyBookStore.Models
+{
+    public class CreateBookRequest
+    {
+        [StringLength(100)]
+        public string Title { get; set; } = string.Empty;
+        [StringLength(100)]
+        public string Author { get; set; } = string.Empty;
+        [Range(0, 10000)]
+        public decimal Price { get; set; }
+        [StringLength(500)]
+        public string Description { get; set; } = string.Empty;
+    }
+}
